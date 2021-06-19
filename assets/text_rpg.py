428b21b529c8e205d5b972720381f1175c9adac5
Ilma Aliya Fiddien <52804524+ilmaaliyaf@users.noreply.github.com>
2021-06-19 23:55:10 +0700

    add assets and screenshots

---
+++ assets/text_rpg.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+from Tkinter import *
+
+root = Tk()
+scrollbar = Scrollbar(root)
+scrollbar.pack( side = RIGHT, fill = Y )
+
+mylist = Listbox(root, yscrollcommand = scrollbar.set )
+for line in range(100):
+   mylist.insert(END, "This is line number " + str(line))
+
+mylist.pack( side = LEFT, fill = BOTH )
+scrollbar.config( command = mylist.yview )
+
+mainloop()
